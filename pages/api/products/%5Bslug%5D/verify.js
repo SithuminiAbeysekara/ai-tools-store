e47746c5525b288391ee6dfaaa0d8512f9e64918
SithuminiAbeysekara <sudeepaabeysekara2002@gmail.com>
2026-04-05 23:43:42 +0530

    fix build errors

--- pages/api/products/%5Bslug%5D/verify.js
+++ pages/api/products/%5Bslug%5D/verify.js
@@ -1,4 +1,4 @@
-import products from '../../../data/products.json'
+import products from '../../../../data/products.json'
 
 export default function handler(req, res) {
   const { slug } = req.query
